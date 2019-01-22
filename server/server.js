eedc898205ba022ae2667b5400817763a1095e61
shubham arya <shubhamarya@shubhams-MacBook-Pro.local>
2019-01-22 17:32:38 -0500

    added PATCH/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const _ = require("lodash");
 const bodyParser = require("body-parser");
 var app = express();
 const { mongoose } = require("./db/mongooose");
@@ -46,7 +47,7 @@ app.get("/todos/:id", (req, res) => {
 });
 app.delete("/todos/:id", (req, res) => {
   var id = req.params.id;
-  if (!ObjectID.isValid) {
+  if (!ObjectID.isValid(id)) {
     res.status(404).send();
   }
   Todo.findByIdAndRemove(id).then(todo => {
@@ -55,6 +56,29 @@ app.delete("/todos/:id", (req, res) => {
     }
     res.status(200).send(todo);
   });
+});
+app.patch("/todos/:id", (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ["text", "completed"]);
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then(todo => {
+      if (!todo) {
+        res.status(404).send();
+      }
+      res.status(200).send({ todo });
+    })
+    .catch(e => {
+      res.status(400).send(e);
+    });
 });
 var port = process.env.PORT || 3000;
 app.listen(port, () => {
